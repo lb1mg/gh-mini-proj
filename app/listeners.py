4da0367bb9dcae40afbe2fc2392f47a00fb7d143
Lakshay Bansal <lakshay.bansal@1mg.com>
2023-07-31 01:02:31 +0530

    FINALY! sorted out redis conn exception

--- app/listeners.py
+++ app/listeners.py
@@ -10,15 +10,29 @@ import redis.asyncio as redis
 before server start
 """
 async def connect_redis(app:Sanic):
-    """ istener for connecting to Redis """
+    """ listener for connecting to Redis """
     # app.ctx.redis = redis.Redis()
-    app.ctx.redis = redis.from_url('redis://localhost')
-    logger.info(f'<<<<< Connected to Redis >>>>>')
+    # app.ctx.redis = redis.from_url('redis://localhost')
+    # app.ctx.redis.ping()
+    app.ctx.redis = None
+    _redis = redis.Redis()
+    try:
+        await _redis.ping()
+    except Exception as e:
+        logger.error(e)
+        logger.error("<<<<< Could not connect to Redis! Shutting Down! >>>>>")
+        app.stop()
+    else:
+        app.ctx.redis = _redis
+        logger.info(f'<<<<< Connected to Redis >>>>>')
 
 """
 before server stop
 """
 async def disconnect_redis(app:Sanic):
     """ listener to close connection to Redis """
-    await app.ctx.redis.close()
+    # await app.ctx.redis.close()
+    _redis = app.ctx.redis
+    if _redis:
+        await _redis.close()
     logger.info(f'<<<<< Disconnected from Redis >>>>>')
