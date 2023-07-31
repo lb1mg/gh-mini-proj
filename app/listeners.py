9be387a3ec6876bada5bf7acdda85c5218fd1d7f
Lakshay Bansal <lakshay.bansal@1mg.com>
2023-07-31 11:12:22 +0530

    added cached sesssion listener

--- app/listeners.py
+++ app/listeners.py
@@ -1,10 +1,13 @@
 """
 LISTENERS
 """
+import redis.asyncio as redis
+from aiohttp_client_cache import CachedSession
+
 from sanic import Sanic
 from sanic.log import logger
 
-import redis.asyncio as redis
+from app.cache import EfficientRedisBackend
 
 """
 before server start
@@ -25,6 +28,20 @@ async def connect_redis(app:Sanic):
     else:
         app.ctx.redis = _redis
         logger.info(f'<<<<< Connected to Redis >>>>>')
+        
+async def create_cached_session(app:Sanic):
+    """ listener to create a persistent CachedSession """
+    app.ctx.session = None
+    _redis = app.ctx.redis
+    _cache_backend = EfficientRedisBackend(
+        connection = _redis,
+        expire_after = 60 * 60
+    )
+    _session = CachedSession(
+        cache=_cache_backend
+    )
+    app.ctx.cached_session = _session
+    logger.info('<<<<< Created Persistent Cached Session >>>>>')
 
 """
 before server stop
@@ -36,3 +53,9 @@ async def disconnect_redis(app:Sanic):
     if _redis:
         await _redis.close()
     logger.info(f'<<<<< Disconnected from Redis >>>>>')
+    
+async def close_cached_session(app:Sanic):
+    _session = app.ctx.cached_session
+    if _session:
+        await _session.close()
+    logger.info('<<<<< Closed Persistent Cached Session >>>>>')
